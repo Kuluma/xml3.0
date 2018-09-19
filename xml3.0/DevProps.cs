f6fb71fda07c9039b8fcb95c5fe08fecb747de96
博一 沈 <a1358115251@hotmail.com>
2018-09-20 00:04:29 +0800

    1102

--- xml3.0/DevProps.cs
+++ xml3.0/DevProps.cs
@@ -12,7 +12,8 @@ namespace xml3._0
     class DevProps
     {
         //string path = FilePath.filePath+ "\\DevProps.xml";
-        string path = @"C:\Users\ShenBoyi\Desktop\大丽线\Station_HDZ\DevProps.xml";
+        string path = @"C:\Users\ShenBY\Desktop\乌将线CTC\Data-北三台+沙地\北三台\Station_BST\DevProps.xml";
+        //string path = @"C:\Users\ShenBY\Desktop\乌将线CTC\Data-北三台+沙地\北三台\Station_BST\LayOut反.xml";
         public DevProps()
         {
 
@@ -156,6 +157,8 @@ namespace xml3._0
 
             doc.Load(path);
             XmlNodeList Activity2Nodes = doc.SelectNodes("DevProps");
+            
+           // XmlNodeList Activity2Nodes = doc.SelectNodes("StationLayout");
             PrintChildNodes(Activity2Nodes);
         }
 
@@ -169,29 +172,37 @@ namespace xml3._0
                 foreach (XmlNode node in childnodelist)
                 {
                     //(node.NodeType 是Text时，即是最内层 即innertext值，node.Attributes为null。
-                    if (node.NodeType == XmlNodeType.Text)
-                    {
-                        Console.WriteLine("NodeType:" + node.NodeType + "\t" + node.Name + "\t:" + node.Value);
-                        
-                        //    List<string> Dev = new List<string>();
-                        //    Dev.Add(node.Name);
-                        //    Dev.Add(node.Value);
-                        //if (node.Name=="name")
-                        //{
-                        //    string name =;
-                        //}
-                        //hashtable_DevProps.Add(node.Name, node.Value);
+                    int a = node.ChildNodes.Count;
+                    //Console.WriteLine("节点数:" + a);
+                    //Console.WriteLine("node.InnerText:"+node.InnerText);
+                    //if (node.NodeType == XmlNodeType.Text)
+                    //{
+                    //    Console.WriteLine("NodeType:" + node.NodeType + "\t" + node.Name + "\t:" + node.Value);
+                    //    Console.WriteLine("214124214124124124124521641278392104");
+                    //    node.SelectSingleNode("name");
+                    //    //    List<string> Dev = new List<string>();
+                    //    //    Dev.Add(node.Name);
+                    //    //    Dev.Add(node.Value);
+                    //    //if (node.Name=="name")
+                    //    //{
+                    //    //    string name =;
+                    //    //}
+                    //    //hashtable_DevProps.Add(node.Name, node.Value);
 
 
 
-                        continue;
-                    }
+                    //    continue;
+                    //}
                     Console.WriteLine("==========node.Name:" + node.Name + "===========");
+                    
                     foreach (XmlAttribute atr in node.Attributes)
                     {
                         Console.WriteLine("NodeType:" + atr.NodeType + "\t" + atr.Name + "\t:" + atr.Value);
+                       // Console.WriteLine(atr.InnerText);
                         //hashtable_DevProps.Add(atr.Name, atr.Value);
+                        List<string> Dev = new List<string>();
                     }
+
                     if (node.ChildNodes.Count > 0)
                     {
                         PrintChildNodes(node.ChildNodes);
